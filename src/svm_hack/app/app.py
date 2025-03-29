5eb1e2ce503ad45c9b02ed8edbf18f68e9b1b0bb
Twomasz <tomasz.wojcick@gmail.com>
2025-03-29 11:50:24 +0100

    fix inputs

--- src/svm_hack/app/app.py
+++ src/svm_hack/app/app.py
@@ -2,13 +2,16 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
 
+from svm_hack.app.utils import st_dtypes
+
+
 
 def main() -> None:
     st.title("Asystent oszczędzania")
 
     st.header("Wprowadź input")
-    selected_func_type = st.selectbox("Target Function", ca.FunctionBox.values())
-    selection_method = st.selectbox("Selection Method", ca.SelectionBox.values())
+    selected_func_type = st.selectbox("Ile masz lat?", st_dtypes.AgeBox.values())
+    selection_method = st.selectbox("W jakim horyzoncie czasowym chcesz zainwestować?", st_dtypes.HorizonttBox.values())
 
 
     # Button to run
